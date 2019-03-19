3d1194c1c881d8369ed97da238377660ee6d707d
Antoine Savelkouls <antoine.savelkouls@sogeti.com>
2019-03-19 07:17:59 +0100

    verhuisdoos en verhuisitem models gemaakt

---
+++ WebAppMVCVerhuisdoos/WebAppMVC/Models/Verhuisdoos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Models
+{
+    public class Verhuisdoos
+    {
+        public int VerhuisdoosId { get; set; }
+
+        public string VerhuisdoosNaam { get; set; }
+
+        public string VerhuisdoosOmschrijving { get; set; }
+
+        public List<Verhuisitem> VerhuisitemLijst { get; set; }
+
+    }
+}
